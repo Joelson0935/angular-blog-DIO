c05d63cc6f4b488899ab984dc2ab15f43b113efa
joelson_luiz <joelsonluiz2010@gmail.com>
2022-11-05 12:35:07 -0300

    Alterações no layout da página

---
+++ src/app/componentes/big-card/big-card.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-big-card',
+  templateUrl: './big-card.component.html',
+  styleUrls: ['./big-card.component.css']
+})
+export class BigCardComponent implements OnInit {
+
+  @Input()
+  imagem: string = ''
+
+  @Input()
+  titulo: string = ''
+
+  @Input()
+  paragrafo: string = ''
+
+  ngOnInit(): void {
+  }
+
+}
